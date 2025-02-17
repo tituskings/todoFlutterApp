6b257a85902834768ef2e32b2ec9115159a32adb
tituskings <kingtitus300@gmail.com>
2025-02-17 05:09:05 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/mainScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Mainscreen(),
+    );
+  }
+}
