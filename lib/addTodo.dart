d7c84051612301abdc0eeeb387bd3a96a2893eed
tituskings <kingtitus300@gmail.com>
2025-02-18 06:40:57 +0100

    second commit

---
+++ lib/addTodo.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class Addtodo extends StatefulWidget {
+  void Function({required String todoText}) addTodo;
+
+  Addtodo({super.key, required this.addTodo});
+
+  @override
+  State<Addtodo> createState() => _AddtodoState();
+}
+
+class _AddtodoState extends State<Addtodo> {
+  TextEditingController todoText = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Add Todo"),
+        TextField(
+          autofocus: true,
+          controller: todoText,
+          decoration: InputDecoration(hintText: "write your todo here"),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              if (todoText.text.isNotEmpty) {
+                widget.addTodo(todoText: todoText.text);
+              }
+              todoText.text = '';
+            },
+            child: Text("Add"))
+      ],
+    );
+  }
+}
--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo_app/addTodo.dart';
 
 class Mainscreen extends StatefulWidget {
   const Mainscreen({super.key});
@@ -8,13 +10,105 @@ class Mainscreen extends StatefulWidget {
 }
 
 class _MainscreenState extends State<Mainscreen> {
+  List<String> todoList = [];
+
+  void addTodo({required String todoText}) {
+    if (todoList.contains(todoText)) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+                title: Text("Already exists"),
+                content: Text("This todo data already exists"),
+                actions: [
+                  InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("close"))
+                ]);
+          });
+    }
+
+    setState(() {
+      todoList.insert(0, todoText);
+    });
+    updateLocalData();
+    Navigator.pop(context);
+  }
+
+  void updateLocalData() async {
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    await prefs.setStringList('todoList', todoList);
+  }
+
+  void loadData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    todoList = (prefs.getStringList("todoList") ?? []).toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Text("drawer data"),
+      ),
       appBar: AppBar(
         centerTitle: true,
         title: Text("Todo App"),
+        actions: [
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return Padding(
+                      padding: MediaQuery.of(context).viewInsets,
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        height: 200,
+                        child: Addtodo(
+                          addTodo: addTodo,
+                        ),
+                      ),
+                    );
+                  });
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.add),
+            ),
+          )
+        ],
       ),
+      body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              onTap: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        padding: EdgeInsets.all(20),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                todoList.removeAt(index);
+                              });
+                              updateLocalData();
+                              Navigator.pop(context);
+                            },
+                            child: Text("Mark as Done")),
+                      );
+                    });
+              },
+              title: Text(todoList[index]),
+            );
+          }),
     );
   }
 }
